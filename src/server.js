930cc5aceeb29297b78ae0899fb919f99b76ace6
avioXD Beast <abhishekdas696legit@gmail.com>
2021-04-15 18:47:29 +0530

    Server changed to 3000

--- src/server.js
+++ src/server.js
@@ -18,7 +18,7 @@ app.set("views", viewsPath);
 hbs.registerPartials(partialsPath);
 
 //** PORT  */
-const PORT = process.env.port || 8000;
+const PORT = process.env.port || 3000;
 app.listen(process.env.PORT || 3000, function() {
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
