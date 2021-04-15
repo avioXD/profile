9ea602746b9e5954478c7aea1296a0770c241e63
avioXD Beast <abhishekdas696legit@gmail.com>
2021-04-15 23:45:42 +0530

    Error fixed 1

--- src/server.js
+++ src/server.js
@@ -38,7 +38,7 @@ app.get('*', (request, response) => {
 });
 
 //** PORT  */s
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3003;
 app.listen(PORT, function() {
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
     console.log('server is running ...');
